affed757fe3b79423fd8a3b4ab5f532c19441090
devgauravs <devgaurav400@gamil.com>
2025-12-17 11:48:46 +0530

    cron

--- src/jobs/checkinCheckoutJob.ts
+++ src/jobs/checkinCheckoutJob.ts
@@ -131,15 +131,6 @@ cron.schedule("* * * * *", async () => {
 //         { _id: checkout._id },
 //         { $set: { isCheckOut: false } }
 //       );
-
-//       // reset checkin to ready state
-//       await CheckIn.updateOne(
-//         {
-//           _id: checkout.checkInID,
-//           user: checkout.user,
-//         },
-//         { $set: { isCheckIn: true } }
-//       );
 //     }
 //   }
 // });
